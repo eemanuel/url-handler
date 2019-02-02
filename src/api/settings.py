a8844c8652d89763be6da356a0df9b97abe8a317
Emanuel Oviedo <eemanueloviedo@gmail.com>
2019-02-01 22:04:07 -0300

    Update gitignore. Create new app user_profile

--- src/api/settings.py
+++ src/api/settings.py
@@ -10,10 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import os
+import sys, os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'user_profile'
 ]
 
 MIDDLEWARE = [
